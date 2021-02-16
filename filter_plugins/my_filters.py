b70f5629ff2fb3ea407e4cc5f72d35110e1dccca
exitspam <exitspam@hotmail.fr>
2021-02-16 09:03:03 +0000

    Ajout de la fonction get_device

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 from natsort import natsorted
 import re
+import subprocess
 
 class FilterModule(object):
     def filters(self):
         return {
             'a_filter': self.a_filter,
-            'latest_version': self.latest_version
+            'latest_version': self.latest_version,
+            'get_device': self.get_device
         }
     def a_filter(self, a_variable):
         a_new_variable = a_variable + ' CRAZY NEW FILTER'
@@ -22,6 +24,5 @@ class FilterModule(object):
                 if m.group(0):
                     break
         return list_of_version
-
-
-
+    def get_device(self,list_device):
+        return list_device
